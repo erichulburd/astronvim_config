cefe76223b37f02bf358ca9af2b162f92def58d7
Eric Hulburd <ehulburd@rigetti.com>
2023-06-09 10:48:31 -0700

    feat: version nlsp settings

--- plugins/lsp.lua
+++ plugins/lsp.lua
@@ -4,8 +4,8 @@ return {
     {
       "tamago324/nlsp-settings.nvim", -- add the nlsp setting plugin
       opts = {
-                                      -- set the config table for the `setup()` call
-        config_home = vim.fn.stdpath "config" .. "/nlsp-settings",
+        -- set the config table for the `setup()` call
+        config_home = vim.fn.stdpath "config" .. "lua/user/nlsp-settings",
         local_settings_dir = ".nlsp-settings",
         local_settings_root_markers_fallback = { ".git" },
         append_default_schemas = true,
