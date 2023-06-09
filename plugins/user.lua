e58080a9f5c4b4d911f1f698a75945c2b64d4673
Eric Hulburd <ehulburd@rigetti.com>
2023-06-09 11:20:40 -0700

    feat: configure python adapter

--- plugins/user.lua
+++ plugins/user.lua
@@ -59,5 +59,24 @@ return {
       })
       require("copilot_cmp").setup()
     end
+  },
+  {
+    "mfussenegger/nvim-dap-python",
+    pin = true,
+    config = function()
+      require("dap-python").setup("~/.local/virtualenvs/debugpy/bin/python")
+      require("dap-python").test_runner = "pytest"
+      require('dap').set_exception_breakpoints({ "raised", "uncaught" })
+
+      require('dap').configurations.python = {
+        {
+          type = 'python',
+          request = 'launch',
+          name = 'My custom launch configuration',
+          justMyCode = false,
+          program = '${file}',
+        },
+      }
+    end,
   }
 }
