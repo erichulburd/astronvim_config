60715e48d5e2eb27cfa80fb9b95de96d025055f7
Eric Hulburd <ehulburd@rigetti.com>
2024-05-02 09:34:03 -0700

    fix: nvm sourcing

--- scripts/install-linux.sh
+++ scripts/install-linux.sh
@@ -19,7 +19,9 @@ cp -r squashfs-root/* $HOME/squashfs-root
 
 # NVM
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-source /home/default-user/.bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 # Node.js
 nvm install --lts
