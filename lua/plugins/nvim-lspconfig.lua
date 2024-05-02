e07a2b9912bed2a7ba4f80a73107f4861b246574
Eric Hulburd <ehulburd@rigetti.com>
2024-05-02 09:25:59 -0700

    feat: support nlsp settings

---
+++ lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,16 @@
+return {
+  "neovim/nvim-lspconfig",
+  dependencies = {
+    {
+      "tamago324/nlsp-settings.nvim", -- add the nlsp setting plugin
+      opts = {
+        -- set the config table for the `setup()` call
+        config_home = vim.fn.stdpath "config" .. "lua/user/nlsp-settings",
+        local_settings_dir = ".nlsp-settings",
+        local_settings_root_markers_fallback = { ".git" },
+        append_default_schemas = true,
+        loader = "json",
+      },
+    },
+  }
+}
