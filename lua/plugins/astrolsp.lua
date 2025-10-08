4c9687c6fe5acc7fbd77b48c293dc689a8e2e219
Eric Hulburd <ehulburd@rigetti.com>
2025-10-08 15:49:19 -0700

    feat: upgrade to astronvim v5

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -1,105 +1,57 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
--- AstroLSP allows you to customize the features in AstroNvim's LSP configuration engine
--- Configuration documentation can be found with `:h astrolsp`
--- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
---       as this provides autocomplete and documentation while editing
+-- For an example see https://github.com/AstroNvim/template/blob/b777fe96c301ac472642dadb9721a7e799700420/lua/plugins/astrolsp.lua
 
 ---@type LazySpec
 return {
   "AstroNvim/astrolsp",
+  -- enable servers that you already have installed without mason
   ---@type AstroLSPOpts
-  opts = {
-    -- Configuration table of features provided by AstroLSP
-    features = {
-      codelens = true, -- enable/disable codelens refresh on start
-      inlay_hints = false, -- enable/disable inlay hints on start
-      semantic_tokens = true, -- enable/disable semantic token highlighting
-    },
-    -- customize lsp formatting options
-    formatting = {
-      -- control auto formatting on save
-      format_on_save = {
-        enabled = true, -- enable or disable format on save globally
-        allow_filetypes = { -- enable format on save for specified filetypes only
-          -- "go",
-        },
-        ignore_filetypes = { -- disable format on save for specified filetypes
-          -- "python",
-        },
-      },
-      disabled = { -- disable formatting capabilities for the listed language servers
-        -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
-        -- "lua_ls",
-      },
-      timeout_ms = 1000, -- default format timeout
-      -- filter = function(client) -- fully override the default formatting function
-      --   return true
-      -- end
-    },
-    -- enable servers that you already have installed without mason
-    servers = {
-      -- "pyright"
-    },
-    -- customize language server configuration options passed to `lspconfig`
-    ---@diagnostic disable: missing-fields
-    config = {
-      -- clangd = { capabilities = { offsetEncoding = "utf-8" } },
-    },
-    -- customize how language servers are attached
-    handlers = {
-      -- a function without a key is simply the default handler, functions take two parameters, the server name and the configured options table for that server
-      -- function(server, opts) require("lspconfig")[server].setup(opts) end
+  opts = function(plugin, opts)
+    -- safely extend the servers list
+    opts.servers = opts.servers or {}
 
-      -- the key is the server that is being setup with `lspconfig`
-      -- rust_analyzer = false, -- setting a handler to false will disable the set up of that language server
-      -- pyright = function(_, opts) require("lspconfig").pyright.setup(opts) end -- or a custom handler function can be passed
-    },
-    -- Configure buffer local auto commands to add when attaching a language server
-    autocmds = {
-      -- first key is the `augroup` to add the auto commands to (:h augroup)
-      lsp_codelens_refresh = {
-        -- Optional condition to create/delete auto command group
-        -- can either be a string of a client capability or a function of `fun(client, bufnr): boolean`
-        -- condition will be resolved for each client on each execution and if it ever fails for all clients,
-        -- the auto commands will be deleted for that buffer
-        cond = "textDocument/codeLens",
-        -- cond = function(client, bufnr) return client.name == "lua_ls" end,
-        -- list of auto commands to set
-        {
-          -- events to trigger
-          event = { "InsertLeave", "BufEnter" },
-          -- the rest of the autocmd options (:h nvim_create_autocmd)
-          desc = "Refresh codelens (buffer)",
-          callback = function(args)
-            if require("astrolsp").config.features.codelens then vim.lsp.codelens.refresh { bufnr = args.buf } end
-          end,
-        },
+    vim.filetype.add({
+      extension = {
+        mojo = 'mojo',
+        cxx = 'cpp',
+        cppm = 'cpp',
+
       },
-    },
-    -- mappings to be set up on attaching of a language server
-    mappings = {
-      n = {
-        -- a `cond` key can provided as the string of a server capability to be required to attach, or a function with `client` and `bufnr` parameters from the `on_attach` that returns a boolean
-        gD = {
-          function() vim.lsp.buf.declaration() end,
-          desc = "Declaration of current symbol",
-          cond = "textDocument/declaration",
-        },
-        ["<Leader>uY"] = {
-          function() require("astrolsp.toggles").buffer_semantic_tokens() end,
-          desc = "Toggle LSP semantic highlight (buffer)",
-          cond = function(client)
-            return client.supports_method "textDocument/semanticTokens/full" and vim.lsp.semantic_tokens ~= nil
-          end,
-        },
-      },
-    },
-    -- A custom `on_attach` function to be run after the default `on_attach` function
-    -- takes two parameters `client` and `bufnr`  (`:h lspconfig-setup`)
-    on_attach = function(client, bufnr)
-      -- this would disable semanticTokensProvider for all clients
-      -- client.server_capabilities.semanticTokensProvider = nil
-    end,
-  },
+    })
+    vim.api.nvim_create_autocmd("FileType", {
+        pattern = "mojo",
+        callback = function()
+            vim.opt_local.commentstring = "# %s"
+        end
+    })
+
+    vim.list_extend(opts.servers, {
+      "mojo",
+      "neocmake",
+      "clangd"
+    })
+
+    opts.config = opts.config or {}
+
+
+    -- https://github.com/neovim/nvim-lspconfig/blob/056f569f71e4b726323b799b9cfacc53653bceb3/doc/server_configurations.md#neocmake
+    local neocmake_capabilities = vim.lsp.protocol.make_client_capabilities()
+    neocmake_capabilities.textDocument.completion.completionItem.snippetSupport = true
+    opts.config.neocmake = { capabilities = neocmake_capabilities }
+
+    local clangd_capabilities = vim.lsp.protocol.make_client_capabilities()
+    clangd_capabilities.offsetEncoding = "utf-16"
+    local llvm_tag = os.getenv("LLVM_TAG")
+    if llvm_tag == nil then llvm_tag = os.getenv("DEFAULT_LLVM_TAG") end
+    opts.config.clangd = {
+      capabilities = clangd_capabilities,
+      cmd = { os.getenv("HOME") .. "/.build/" .. llvm_tag .. "/bin/clangd" },
+      filetypes = { "c", "cpp", "cppm", "cxx", "objc", "objcpp", "cuda" }
+    }
+  
+    -- See https://github.com/hrsh7th/cmp-nvim-lsp/issues/44#issuecomment-2096368152
+    -- local cmp_nvim_lsp = require("cmp_nvim_lsp")
+    -- local rust_analyzer_capabilities = cmp_nvim_lsp.default_capabilities()
+    -- rust_analyzer_capabilities.workspace = { didChangeWatchedFiles = { dynamicRegistration = true } }
+    -- opts.config.rust_analyzer = { capabilities = rust_analyzer_capabilities }
+  end,
 }
